bd66fbe4b6093fdccce537dd3681f1a369f31f3a
Rakibul Islam <rakibul.islam@s2019.tu-chemnitz.de>
2021-03-16 21:11:28 +0100

    Event-bus added

---
+++ event-bus/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+const events = [];
+
+app.frontend('/events', (req, res) => {
+  const event = req.body;
+
+  events.push(event);
+
+  axios.post('http://""/events', event);
+  axios.post('http://""/events', event);
+  axios.post('http://""/events', event);
+  axios.post('http://""/events', event);
+
+  res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
+});
+
+app.listen(4005, () => {
+  console.log('Listening on 4005');
+});
