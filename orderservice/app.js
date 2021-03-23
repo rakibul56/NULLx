2808afb91800f0000af7f8c9ee66f73aa93377fc
Meshu Deb Nath <meshu-deb.nath@s2019.tu-chemnitz.de>
2021-03-23 14:07:19 +0100

    order service edited

--- orderservice/app.js
+++ orderservice/app.js
@@ -1,25 +1,132 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3009;
 var mysql = require('mysql');
+const cors = require('cors');
+var bodyParser = require('body-parser');
 
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: ""
-});
+app.use(cors());
+app.use(bodyParser.json());
 
-con.connect(function(err) {
+var amqp = require('amqplib/callback_api');
+const amqp_url = 'amqp://localhost'; //for local environment
+//const amqp_url = 'amqp://localhost'; //for server
+
+//var con = mysql.createConnection('mysql://root:1234@10.105.125.0:3306/product_db');
+var con = mysql.createConnection('mysql://root:@localhost/order_list_db');
+
+con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
 });
 
+function amqpMethod(msg){
+    amqp.connect(amqp_url, function (error0, connection) {
+        if (error0) {
+            console.log( error0 );
+        }
+        /*
+        * Channel for sending Data
+        * */
+        connection.createChannel(function (error1, channel) {
+            if (error1) {
+                console.log( error1 );
+            }
 
+            var orderCreated = 'orderCreated';
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+            var msg = {order_id: obj.order_id, status: obj.status};
+            msg = JSON.stringify(msg);
+
+            channel.assertQueue(orderCreated, {
+                durable: false
+            });
+            channel.sendToQueue(orderCreated, Buffer.from(msg));
+
+        }); /* ending of channel for sending data */
+
+        connection.createChannel(function (error1, channel) {
+            if (error1) {
+                console.log( error1);
+            }
+
+            var orderStatus = 'orderStatus';
+            channel.assertQueue(orderStatus, {
+                durable: false
+            });
+
+            channel.consume(orderStatus, function (msg) {
+                    //console.log(" [x] Received %s", msg.content.toString());
+                    let msg_json = JSON.parse(msg.content);
+                    console.log("Event 1: " + msg_json.product_id);
+
+                    var queryStr = "SELECT * FROM orders WHERE order_id = '" + msg_json.order_id + "'";
+                    con.query(queryStr, function (error, result, fields) {
+                        if (error) {
+                            console.log(error);
+                        }
+                        if (result.length > 0) {
+                            //console.log("data" + JSON.stringify(data));
+
+                        }
+                    });
+                }
+                , {
+                    noAck: true
+                });
+
+        });
+    });
+}
+
+app.get('/orders', cors(), (req, res) => {
+    var queryStr = "SELECT * FROM orders WHERE 1";
+    con.query(queryStr, function (error, result, fields) {
+        if (error) {
+            console.log(error);
+            res.send("not found");
+        }
+        //console.log("data" + JSON.stringify(data));
+        res.status(200);
+        res.send(result);
+        //console.log(res);
+    });
+});
+
+app.get('/orders/:id', cors(), (req, res) => {
+    const item = req.params.id;
+    //console.log("is number: " + !isNaN(item));
+    if (item) {
+        console.log("search key: " + item);
+        var queryStr = "SELECT * FROM orders WHERE orders.payment_id LIKE '" + item + "%' OR orders.order_id LIKE '" + item + "%' OR orders.cart_id LIKE '" + item + "%' OR orders.address LIKE '" + item + "%'";
+        con.query(queryStr, function (error, result, fields) {
+            if (error) {
+                console.log(error);
+                if (error.code == 'ER_BAD_FIELD_ERROR') {
+                    res.status(400);
+                    res.send("Bad request. Please check the data format of the product id.");
+                }
+                res.status(404);
+                res.send("Please check the requested path. Or bad request can not proceed");
+            }
+            if (result.length > 0) {
+                if (result) {
+                    res.status(200);
+                    res.send(result);
+                }
+            } else {
+                console.log("not found: " + result);
+                res.status(404);
+                res.send("Not Found");
+            }
+        });
+    } else {
+        res.status(400);
+        res.send("Bad request. Please check the data format of the product id.");
+    }
+});
+
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Inventory Service listening at http://localhost:${port}`)
+});
