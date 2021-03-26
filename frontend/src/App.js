c45ac83ccea0a9ecd906154743ba6e1040a2077d
Meshu Deb Nath <meshu-deb.nath@s2019.tu-chemnitz.de>
2021-03-26 07:34:20 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,7 +28,7 @@ class App extends React.Component {
                     </Switch>
                     <div className="footer-container">
                         <div className="footer container">
-                            <p>This website is hosted for demonstration purposes only. It is not an actual shop.</p>
+                            <p>This website is hosted for demonstration purposes only. It is not an actual online shop.</p>
 
                             <p>&copy; 2021 NULLx (Planspiel Web Engineering)</p>
                         </div>
